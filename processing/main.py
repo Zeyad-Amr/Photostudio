d955fc4b6a4928ed898138c8c1be981293805e96
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2023-03-06 21:19:41 +0200

    refactoring structure

--- processing/main.py
+++ processing/main.py
@@ -1,23 +1,20 @@
-import Greyscale_operator
-from Greyscale_operator import GrayScaleOperator
-
-import Colored_operator
-from Colored_operator import ColoredOperator
+from Histograms import Histograms
+from Histograms import ColoredOperator
 
 import cv2
 import numpy as np
 # from matplotlib import pyplot as plt
 
-img = cv2.imread('processing\lena.jpg',0)
+img = cv2.imread('../images/lena.jpg', 0)
 
-imgOperator = GrayScaleOperator(img)
-newImg =  imgOperator.applyGlobalThreshold(50)
-cv2.imwrite('processing\localthreshold.jpg',newImg)
+imgOperator = Histograms(img)
+newImg = imgOperator.applyGlobalThreshold(50)
+cv2.imwrite('../images/localthreshold.jpg', newImg)
 
-img = cv2.imread('processing\Red_Color.jpg')
+img = cv2.imread('../images/Red_Color.jpg')
 imgOperator = ColoredOperator(img)
-r,g,b = imgOperator.split()
-print(r),print(g),print(b)
+r, g, b = imgOperator.split()
+print(r), print(g), print(b)
 
 
 # plt.imshow(img,cmap='gray')

8095e5a121b9cc3bd3a54232e497b22c73602560
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2023-03-07 01:20:31 +0200

    noise

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { FileContextProvider } from './Components/contexts/fileContext';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <FileContextProvider> */}
+    <FileContextProvider>
     <App />
-    {/* </FileContextProvider> */}
+    </FileContextProvider>
   </React.StrictMode>
 );
 

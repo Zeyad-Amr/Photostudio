a3a5bdf2ca4d3a11dba8a92cf606f9355f6c8c81
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2023-03-09 19:38:08 +0200

    canny edge detection algorithm

--- backend/processing/Filters.py
+++ backend/processing/Filters.py
@@ -1,27 +1,5 @@
-import cv2
 from math import sqrt
 import numpy as np
-
-""" 
-# TODO:
-1. Add additive noise to the image
-- Uniform
-- Gaussian
-- Salt and Pepper 
-
-2. Filter the noisy image using the following low pass filter: (Smooting)
-- Average
-- Gaussian
-- Median
-
-3. Detect the edges of the image using the following masks: 
-- Sobel
-- Roberts
-- Prewitt
-- Canny
-
-
-"""
 
 
 class Filters:
@@ -132,9 +110,25 @@ class Filters:
         horizontal_grad_filter = np.array([[0, 0, 0], [0, 0, 1], [0, -1, 0]])
         return self.__detect_edges_helper(image, vertical_grad_filter, horizontal_grad_filter)
 
+    def canny_edge_detector(self, image, range):
+        # apply gaussian filter
+        image = self.gaussian_filter(image, 5)
+
+        # apply sobel edge detector
+        image = self.sobel_edge_detector(image)
+
+        # apply non-maximum suppression
+        image = self.__non_maximum_suppression(image)
+
+        # # apply double threshold & hysteresis
+        image = self.__double_threshold_hysteresis(image, 0, range)
+
+        return image
+
+    ######################################################################
+    # Edge detection Helper functions
+
     def __detect_edges_helper(self, image, vertical_grad_filter=None, horizontal_grad_filter=None):
-        # convert to grayscale image
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         # normalize the image
         image = image/255
@@ -144,12 +138,17 @@ class Filters:
 
         # initialize the gradient image
         gradient = np.zeros(image.shape)
+        self.angles = np.zeros(image.shape)
 
         # pad the image
         image = np.pad(image, kernel_width, 'constant')
 
-        for i in range(kernel_width, image.shape[0] - kernel_width):
-            for j in range(kernel_width, image.shape[1] - kernel_width):
+        # get the image dimensions
+        rows, cols = image.shape
+
+        # apply the filter
+        for i in range(kernel_width, rows - kernel_width):
+            for j in range(kernel_width, cols - kernel_width):
 
                 # obtain the horizontal gradients
                 x = image[i - kernel_width: i + kernel_width +
@@ -166,5 +165,86 @@ class Filters:
                 # calculate the gradient
                 gradient[i - kernel_width][j -
                                            kernel_width] = sqrt(sum_x**2 + sum_y**2)
+                self.angles[i - kernel_width][j -
+                                              kernel_width] = np.arctan2(sum_y, sum_x)
 
         return gradient
+
+    def __non_maximum_suppression(self, image):
+        size = image.shape
+
+        # convert angles to degrees
+        angles = self.angles * 180. / np.pi
+
+        # remove negative angles and avoid 'numpy.float64' object does not support item assignment
+        angles[angles < 0] += 180
+
+        # create a zero matrix with the same dimensions as the image
+        suppressed = np.zeros(size)
+
+        # loop through the image
+        for i in range(1, size[0] - 1):
+            for j in range(1, size[1] - 1):
+                # get the angle of the pixel
+                if (0 <= angles[i, j] < 22.5) or (157.5 <= angles[i, j] <= 180):
+                    value_to_compare = max(image[i, j - 1], image[i, j + 1])
+                elif (22.5 <= angles[i, j] < 67.5):
+                    value_to_compare = max(
+                        image[i - 1, j - 1], image[i + 1, j + 1])
+                elif (67.5 <= angles[i, j] < 112.5):
+                    value_to_compare = max(image[i - 1, j], image[i + 1, j])
+                else:
+                    value_to_compare = max(
+                        image[i + 1, j - 1], image[i - 1, j + 1])
+
+                # compare the pixel to its neighbours in the direction of the gradient
+                if image[i, j] >= value_to_compare:
+                    suppressed[i, j] = image[i, j]
+
+        # scale the image to 0-255
+        suppressed = np.multiply(suppressed, 255.0 / suppressed.max())
+
+        return suppressed
+
+    def __double_threshold_hysteresis(self, image, low, high):
+
+        weak = 50
+        strong = 255
+
+        # get the dimensions of the image
+        rows, cols = image.shape
+
+        # create a zero matrix with the same dimensions as the image
+        result = np.zeros((rows, cols))
+
+        # get the indices of the pixels that are above the low threshold
+        weak_x, weak_y = np.where((image > low) & (image <= high))
+
+        # get the indices of the pixels that are above the high threshold
+        strong_x, strong_y = np.where(image >= high)
+
+        # set the pixels that are above the high threshold to strong
+        result[strong_x, strong_y] = strong
+
+        # set the pixels that are above the low threshold to weak
+        result[weak_x, weak_y] = weak
+
+        # dx , dy are the directions to check for neighbours
+        dx = np.array((-1, -1, 0, 1, 1, 1, 0, -1))
+        dy = np.array((0, 1, 1, 1, 0, -1, -1, -1))
+
+        # hysteresis
+        while len(strong_x):
+            x = strong_x[0]
+            y = strong_y[0]
+            strong_x = np.delete(strong_x, 0)
+            strong_y = np.delete(strong_y, 0)
+            for direction in range(len(dx)):
+                new_x = x + dx[direction]
+                new_y = y + dy[direction]
+                if ((new_x >= 0 & new_x < rows & new_y >= 0 & new_y < cols) and (result[new_x, new_y] == weak)):
+                    result[new_x, new_y] = strong
+                    np.append(strong_x, new_x)
+                    np.append(strong_y, new_y)
+        result[result != strong] = 0
+        return result

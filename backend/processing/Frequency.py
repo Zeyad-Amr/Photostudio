0d4714cc9483980fb80c443a2a009d4e6d802bbb
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2023-03-09 19:42:26 +0200

    remove grey scale conversion in functions

--- backend/processing/Frequency.py
+++ backend/processing/Frequency.py
@@ -7,19 +7,16 @@ class Frequency:
     def __init__(self):
         pass
 
-    def high_pass_filter(self,img, filter_range):
+    def high_pass_filter(self, img, filter_range):
         # resize the image
         img = cv2.resize(img, (512, 512))
 
-        # convert to grayscale image
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
         # apply the Fourier transform to the image
-        f = np.fft.fft2(gray)
+        f = np.fft.fft2(img)
         fshift = np.fft.fftshift(f)
 
         # define the filter mask
-        rows, cols = gray.shape
+        rows, cols = img.shape
         crow, ccol = rows//2, cols//2
         mask = np.zeros((rows, cols), np.uint8)
 
@@ -39,19 +36,16 @@ class Frequency:
 
         return filtered_image
 
-    def low_pass_filter(self,img, filter_range):
+    def low_pass_filter(self, img, filter_range):
         # resize the image
         img = cv2.resize(img, (512, 512))
 
-        # convert to grayscale image
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
         # apply the Fourier transform to the image
-        f = np.fft.fft2(gray)
+        f = np.fft.fft2(img)
         fshift = np.fft.fftshift(f)
 
         # define the filter mask
-        rows, cols = gray.shape
+        rows, cols = img.shape
         crow, ccol = rows//2, cols//2
         mask = np.zeros((rows, cols), np.uint8)
 
@@ -71,5 +65,5 @@ class Frequency:
 
         return filtered_image
 
-    def hypridImages(self,img1, img2):
+    def hypridImages(self, img1, img2):
         return img1+img2

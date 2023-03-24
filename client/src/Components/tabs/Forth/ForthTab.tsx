5a0baa3cf0563af7ec45ec84597833ba9165174a
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2023-03-24 13:48:36 +0200

    forthTab without integrate with back

--- client/src/Components/tabs/Forth/ForthTab.tsx
+++ client/src/Components/tabs/Forth/ForthTab.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
-import UploadContour from './UploadContour'
 import UploadImg from './UploadImg'
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
@@ -8,6 +7,7 @@ import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import Slider from '@mui/material/Slider';
 import './ForthTab.css'
+import axios from '../../global/API/axios';
 
 
 const ForthTab = () => {
@@ -15,29 +15,47 @@ const ForthTab = () => {
     const [options, setOptions] = useState<string>('');
     const [minRadius, setMinRadius] = useState<number>(25)
     const [maxRadius, setMaxRadius] = useState<number>(40)
-    const [threshold, setThreshold] = useState<number>(10)
+    const [circleThreshold, setCircleThreshold] = useState<number>(10)
+    const [lineThreshold, setLineThreshold] = useState<number>(10)
+    const [spinnerFlag, setSpinnerFlag] = useState<boolean | null>(false)
+
 
 
     const handleChangeOptions = (event: SelectChangeEvent) => {
         setOptions(event.target.value);
     };
 
+    // integrating with back
+
+    const sendRequest = (option: string, firstValue: number, secondValue?: number, thirdValue?: number) => {
+        setSpinnerFlag(true)
+        axios.post("", {
+            option, firstValue, secondValue, thirdValue
+        }).then((response : any) => {
+            setSpinnerFlag(false)
+            console.log(response)
+        }).catch((err : string) => {
+            console.log(err)
+        })
+    }
+
+    const handleLineClick = () => {
+        sendRequest(options, lineThreshold)
+    }
+
+    const handleCircleClick = () => {
+        sendRequest(options, minRadius, maxRadius, circleThreshold)
+    }
+
     return (
         <Container fluid>
             <Row>
                 <Col style={{ height: "85vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }} lg={4} md={6} sm={12} xs={12}>
-                    <UploadContour />
-                    <div className='output-container'>
-                        <label className='output-label uploadimg' >Contour Output</label>
-                        <div className='output-img-contain'>
-                            <img className='output-img' alt="" />
-                        </div>
-                    </div>
+                    <UploadImg />
                 </Col>
                 <Col style={{ height: "85vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }} lg={4} md={6} sm={12} xs={12}>
-                    <UploadImg />
                     <FormControl style={{ marginTop: "2rem", width: "14rem" }} variant="standard" sx={{ m: 1, minWidth: 150 }}>
-                        <InputLabel id="demo-simple-select-autowidth-label">Choose shape detector</InputLabel>
+                        <InputLabel id="demo-simple-select-autowidth-label">Choose detector</InputLabel>
                         <Select
                             labelId="demo-simple-select-autowidth-label"
                             id="demo-simple-select-autowidth"
@@ -51,45 +69,62 @@ const ForthTab = () => {
                             <MenuItem value="1">Lines</MenuItem>
                             <MenuItem value="2">Circles</MenuItem>
                             <MenuItem value="3">Ellipse</MenuItem>
+                            <MenuItem value="4">Active Contour</MenuItem>
                         </Select>
                     </FormControl>
+                    {
+                        options === "1" ?
+                            <div className='btn-slider-contain forthtab'>
+                                <div className='sliders-contain'>
+                                    <label htmlFor="">Threshold</label>
+                                    <Slider value={lineThreshold} onChange={(e: any) => setLineThreshold(e.target.value)} min={0} max={100} step={1} style={{ width: "11rem" }} aria-label="Default" valueLabelDisplay="auto" />
+                                </div>
+                                <button className='apply-btn' onClick={handleLineClick}>Apply</button>
+                            </div>
+
+                            : options === "2" ?
+                                <div className='btn-slider-contain forthtab'>
+                                    <div className='sliders-contain'>
+                                        <label htmlFor="">Min Radius</label>
+                                        <Slider value={minRadius} onChange={(e: any) => setMinRadius(e.target.value)} min={0} max={100} step={1} style={{ width: "11rem" }} aria-label="Default" valueLabelDisplay="auto" />
+                                    </div>
+                                    <div className='sliders-contain'>
+                                        <label htmlFor="">Max Radius</label>
+                                        <Slider value={maxRadius} onChange={(e: any) => setMaxRadius(e.target.value)} min={0} max={100} step={1} style={{ width: "11rem" }} aria-label="Default" valueLabelDisplay="auto" />
+                                    </div>
+                                    <div className='sliders-contain'>
+                                        <label htmlFor="">Threshold</label>
+                                        <Slider value={circleThreshold} onChange={(e: any) => setCircleThreshold(e.target.value)} min={0} max={100} step={1} style={{ width: "11rem" }} aria-label="Default" valueLabelDisplay="auto" />
+                                    </div>
+                                    <button className='apply-btn' onClick={handleCircleClick}>Apply</button>
+                                </div>
+                                : null
+                    }
                 </Col>
                 <Col style={{ height: "85vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }} lg={4} md={12} sm={12} xs={12}>
                     <div className='output-container'>
                         <label className='output-label uploadimg' >
                             {
                                 options === "1" ?
-                                "Lines output "
-                                : options === "2" ? 
-                                "Circles Output"
-                                : options === "3" ?
-                                "Ellipse Output"
-                                : "Output"
+                                    "Lines output "
+                                    : options === "2" ?
+                                        "Circles Output"
+                                        : options === "3" ?
+                                            "Ellipse Output"
+                                            : options === "4" ?
+                                                "Contour Output"
+                                                : "Output"
                             }
-                            </label>
+                        </label>
                         <div className='output-img-contain'>
-                            <img className='output-img' alt="" />
+                            {
+                                spinnerFlag ?
+                                    <div className="spinner-border" role="status" ></div>
+                                    :
+                                    <img className='output-img' alt="" />
+                            }
                         </div>
                     </div>
-                    {
-                        options === "2" ?
-                            <div className='btn-slider-contain forthtab'>
-                                <div className='sliders-contain'>
-                                    <label htmlFor="">Min Radius</label>
-                                    <Slider value={minRadius} onChange={(e: any) => setMinRadius(e.target.value)} min={0} max={100} step={1} style={{ width: "13rem" }} aria-label="Default" valueLabelDisplay="auto" />
-                                </div>
-                                <div className='sliders-contain'>
-                                    <label htmlFor="">Max Radius</label>
-                                    <Slider value={maxRadius} onChange={(e: any) => setMaxRadius(e.target.value)} min={0} max={100} step={1} style={{ width: "13rem" }} aria-label="Default" valueLabelDisplay="auto" />
-                                </div>
-                                <div className='sliders-contain'>
-                                    <label htmlFor="">Threshold</label>
-                                    <Slider value={threshold} onChange={(e: any) => setThreshold(e.target.value)} min={0} max={100} step={1} style={{ width: "13rem" }} aria-label="Default" valueLabelDisplay="auto" />
-                                </div>
-                                <button className='apply-btn'>Apply</button>
-                            </div>
-                            : null
-                    }
                 </Col>
             </Row>
         </Container>
--- client/src/Components/tabs/Forth/UploadImg.tsx
+++ client/src/Components/tabs/Forth/UploadImg.tsx
@@ -1,22 +1,26 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import axios from '../../global/API/axios';
 
 const UploadImg = () => {
 
     const inputFile = useRef<HTMLInputElement | null>(null);
+    const [spinnerFlag, setSpinnerFlag] = useState<boolean | null>(false)
 
-    //   Integrate with back
+
+    //   Integrating with back
 
     const handleUpload = (e: any) => {
-        //     const formData = new FormData();
-        //     formData.append("image", e.target.files[0])
-        //     axios.post('/image/',
-        //     formData
-        //     ).then((res: any) => {
-        //       console.log(res)
-        //     }).catch((err: any) => {
-        //         console.log(err)
-        //     })
+        const formData = new FormData();
+        formData.append("image", e.target.files[0])
+        setSpinnerFlag(true)
+        axios.post('/image/',
+            formData
+        ).then((res: any) => {
+            setSpinnerFlag(false)
+            console.log(res)
+        }).catch((err: any) => {
+            console.log(err)
+        })
     }
 
     const handleButtonClick = () => {
@@ -37,8 +41,13 @@ const UploadImg = () => {
             <button className='upload-btn' onClick={handleButtonClick}>
                 Upload
             </button>
-            <div className='upload-img-contain'>
-                <img className='upload-img' alt="" />
+            <div className='upload-img-contain forthTab'>
+                {
+                    spinnerFlag ?
+                        <div className="spinner-border" role="status"></div>
+                        :
+                        <img className='upload-img' alt="" />
+                }
             </div>
         </div>
     )
